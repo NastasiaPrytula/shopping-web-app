3ba5c76cdbf44c7f7de3e9258e591dbea2f8f119
FominSerhii <fominsergeyo15@gmail.com>
2018-12-25 18:13:34 +0200

    access to admin-panel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,11 +11,12 @@ import { ProductsComponent } from './products/products.component';
 import { FavouriteComponent } from './favourite/favourite.component';
 import { CartComponent } from './cart/cart.component';
 import { PageNotFoundComponent } from'./page-not-found/page-not-found.component';
+import { AuthGuard } from './sign-up/auth-guard.service';
 
 const routes: Routes = [
   { path: 'signup', component: SignUpComponent },
   { path: 'signin', component: SignInComponent},
-  { path: 'admin-products', component: AdminProductsComponent },
+  { path: 'admin-products', component: AdminProductsComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: 'products' , component: ProductsComponent },
   { path: 'favourite', component: FavouriteComponent },
